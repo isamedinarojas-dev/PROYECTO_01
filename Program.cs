bb0e5e92e8c682e0d1e48aaa73dc43185688d4ad
Isaias Medina <isamedinarojas@gmail.com>
2025-08-24 17:33:47 -0500

    Primera version

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Proyecto_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Curso FUNAL");
+
+            Console.ReadKey();
+        }
+    }
+}
